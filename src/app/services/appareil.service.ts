62fcabd1255d8f6650ed8d081a3744faa331b22e
sergisergio <ptraon@gmail.com>
2020-03-19 15:49:46 +0100

    fix vulnerabilities

--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -7,23 +7,23 @@ export class AppareilService {
 
   appareilsSubject = new Subject<any[]>();
 
-  private appareils = [];
-
-    /*{
-    id: 1,
-    name: 'Machine à laver',
-    status: 'éteint'
-    },
+  private appareils = [
     {
-    id: 2,
-    name: 'Frigo',
-    status: 'allumé'
-    },
-    {
-    id: 3,
-    name: 'Ordinateur',
-    status: 'éteint'
-    }*/
+      id: 1,
+      name: 'Machine à laver',
+      status: 'éteint'
+      },
+      {
+      id: 2,
+      name: 'Frigo',
+      status: 'allumé'
+      },
+      {
+      id: 3,
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
 
   constructor(private httpClient: HttpClient) {}
 
